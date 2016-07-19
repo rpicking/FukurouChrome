db50aad4aef7d911445967e5c5ba6ac660c6ddc3
Robert Picking <rpicking@umich.edu>
2016-07-19 16:02:40 -0400

    Added header and refresh button.  TODO: fix but where unfollowing stream doesn't remove them from display.  Changing game doesn't remove stream from old game.

--- app/js/popup.js
+++ app/js/popup.js
@@ -1,16 +1,22 @@
 //This is to communicate between popup.js and background.js (eventPage.js)
 //document.addEventListener('DOMContentLoaded', function () { });
 
+function init() {
+    console.log("im here");
+    var htmlContent = '';
+    var streamResults = document.getElementById("streams");
+    htmlContent = chrome.extension.getBackgroundPage().content.createHTML();
+    streamResults.innerHTML = htmlContent;
+}
 
-var htmlContent = '';
-var streamResults = document.getElementById("extension");
-htmlContent = chrome.extension.getBackgroundPage().content.createHTML();
-streamResults.innerHTML = htmlContent;
-//var streamCount = chrome.extension.getBackgroundPage().content.streamCount;
-//if(streamCount > 9) {
-//    chrome.browserAction.setBadgeText({ text: "10+" });
-//}
-//else {
-//    chrome.browserAction.setBadgeText({ text: String(streamCount) });
-//}
+document.addEventListener('DOMContentLoaded', function () {
+    var refresh = document.getElementById("refresh");
+    refresh.addEventListener('click', function () {
+        chrome.extension.getBackgroundPage().start();
+        setTimeout(function () {
+            location.reload();
+        }, 1000);
+    });
+});
 
+init();
